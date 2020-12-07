0555089beab9536e71d0b9822fc9e81d3fccf395
forteip <commaclone@gmail.com>
2020-12-06 22:58:42 -0500

    added world creation and movement

---
+++ Engine/Factories/WorldFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+            World newWorld = new World();
+            newWorld.AddLocation(0, -1, "Home", 
+                "This is your home", 
+                @"C:\Users\Spare Gaming\Desktop\Git\SimpleRPG\Engine\images\locations\home.jpg");
+
+            newWorld.AddLocation(-2, -1, "Farmer's Field",
+                "There are rows of corn growing here, with giant rats hiding between them.",
+                @"C:\Users\Spare Gaming\Desktop\Git\SimpleRPG\Engine\images\locations\Farm_field.jpg");
+
+            newWorld.AddLocation(-1, -1, "Farmer's House",
+                "This is the house of your neighbor, Farmer Ted.",
+                @"C:\Users\Spare Gaming\Desktop\Git\SimpleRPG\Engine\images\locations\Farmhouse.jpg");
+
+            newWorld.AddLocation(-1, 0, "Trading Shop",
+                "The shop of Susan, the trader.",
+                @"C:\Users\Spare Gaming\Desktop\Git\SimpleRPG\Engine\images\locations\Trade_shop.jpg");
+
+            newWorld.AddLocation(0, 0, "Town square",
+                "This is the town square, you rest under a large tree.",
+                @"C:\Users\Spare Gaming\Desktop\Git\SimpleRPG\Engine\images\locations\Town_center.jpg");
+
+            newWorld.AddLocation(1, 0, "Town Gate",
+                "There is a gate here, protecting the town from giant spiders.",
+                @"C:\Users\Spare Gaming\Desktop\Git\SimpleRPG\Engine\images\locations\Town_gate.jpg");
+
+            newWorld.AddLocation(2, 0, "Spider Forest",
+                "The trees in this forest are covered with spider webs.",
+                @"C:\Users\Spare Gaming\Desktop\Git\SimpleRPG\Engine\images\locations\Forest.jpg");
+
+            newWorld.AddLocation(0, 1, "Herbalist's hut",
+                "You see a small hut, with plants drying from the roof.",
+                @"C:\Users\Spare Gaming\Desktop\Git\SimpleRPG\Engine\images\locations\Herbalist_hut.jpg");
+
+            newWorld.AddLocation(0, 2, "Herbalist's garden",
+                "There are many plants here, with snakes hiding behind them.",
+                @"C:\Users\Spare Gaming\Desktop\Git\SimpleRPG\Engine\images\locations\Herb_Garden.jpg");
+            return newWorld;
+        }
+    }
+}
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,14 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 using Engine.Models;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
-    public class GameSession
+    public class GameSession : INotifyPropertyChanged
     {
+        private Location _currentLocation;
         public Player CurrentPlayer { get; set; }
-        public Location CurrentLocation { get; set; }
+        public World CurrentWorld { get; set; }
+        public Location CurrentLocation
+        {
+            get { return _currentLocation; }
+            set
+            {
+                _currentLocation = value;
+                OnPropertyChanged("CurrentLocation");
+                OnPropertyChanged("HasLocationNorth");
+                OnPropertyChanged("HasLocationSouth");
+                OnPropertyChanged("HasLocationEast");
+                OnPropertyChanged("HasLocationWest");
+            }
+        }
+
+        public bool HasLocationNorth
+        {
+            get {
+                return CurrentWorld.locationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
+                }
+        }
+
+        public bool HasLocationSouth
+        {
+            get
+            {
+                return CurrentWorld.locationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
+            }
+        }
+
+        public bool HasLocationEast
+        {
+            get
+            {
+                return CurrentWorld.locationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+
+        public bool HasLocationWest
+        {
+            get
+            {
+                return CurrentWorld.locationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+
 
         public GameSession()
         {
@@ -20,13 +68,34 @@ namespace Engine.ViewModels
             CurrentPlayer.EXP = 0;
             CurrentPlayer.Level = 1;
 
-            CurrentLocation = new Location();
-            CurrentLocation.Name = "Home";
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = -1;
-            CurrentLocation.Description = "This is your House";
-            CurrentLocation.ImageName = @"C:\Users\Spare Gaming\Desktop\Git\SimpleRPG\Engine\images\locations\home.jpg";
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
 
+            CurrentLocation = CurrentWorld.locationAt(0, 0);
+        }
+
+        public void MoveNorth()
+        {
+            CurrentLocation = CurrentWorld.locationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+        }
+        public void MoveSouth()
+        {
+            CurrentLocation = CurrentWorld.locationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
+        public void MoveEast()
+        {
+            CurrentLocation = CurrentWorld.locationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+        }
+        public void MoveWest()
+        {
+            CurrentLocation = CurrentWorld.locationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
